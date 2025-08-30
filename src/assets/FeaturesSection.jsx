ce7184c5608a9cc2a5bcf2643f17c08747e9462d
Snehalkale24 <snehalbkale2004@gmail.com>
2025-08-30 11:58:39 +0530

    improve icon size and color

--- src/assets/FeaturesSection.jsx
+++ src/assets/FeaturesSection.jsx
@@ -1,39 +1,40 @@
-import { HandCoins, Bike , CalendarCheck,LifeBusy } from "lucide-react"
+import { HandCoins, Bike , CalendarCheck,Shield } from "lucide-react"
 const FEATURES_CONFIG = [
   {
     title: "Low Prices",
     description: "we offer the most competitive prices in the market.",
-    icon: <HandCoins />,
+    icon: <HandCoins className="text-red-500 h-8 w-20 mask-l-from-1.5"/>,
   },
   {
     title: "Wide selection ",
     description: "choose from a variety of bicycle to suit your needs",
-    icon: <Bike />,
+    icon: <Bike className="text-blue-800 h-8 w-20 mask-l-from-1.5"/>,
   },
   {
     title : "Easy Booking",
     description:"Book your bicycle online in just a  few clicks.",
-    icon: <CalendarCheck/>
+    icon: <CalendarCheck className="text-yellow-800 h-8 w-20 mask-l-from-1 "/>
   },
   {
     title : "Safety First",
     description:"All our bicycles are regulary maintained for your safety.",
-    icon: <LifeBusy/>
+    icon: <Shield className="text-green-500 h-8 w-20 mask-l-from-1"/>
   },
 ];
 
 function FeaturesSection() {
   return (
     <div>
-      <h2 className='text-center text-30xl text-orange-950'>Why Choose Our Bicycle Rental Service?</h2>
+      <h2 className='text-center text-30xl text-orange-950 '>Why Choose Our Bicycle Rental Service?</h2>
 
 
-      <div className="flex m-10 p-3">{FEATURES_CONFIG.map((feactureObj) => {
+      <div className="flex m-10 ">{FEATURES_CONFIG.map((feactureObj) => {
         const { title, description, icon } = feactureObj;
 
         return (
-          <div key={title}className="m-3 p-4 bg-orange-200 ">{icon}
-          <h3>{title}</h3>
+          <div key={title}className="m-3 p-4 bg-orange-200 ">
+            {icon}
+          <h3 className="font-bold font-serif p-2">{title}</h3>
           <p>{description}</p>
           </div>
         );
